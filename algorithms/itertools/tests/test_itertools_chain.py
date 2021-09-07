6f19a8dcccd35b54ef3e5ca81f32f83b0c709715
Bjorn Lindi <blindij@users.noreply.github.com>
2021-09-07 12:38:44 +0200

    Add test of itertools chain()

--- algorithms/itertools/tests/test_itertools_chain.py
+++ algorithms/itertools/tests/test_itertools_chain.py
@@ -1,7 +1,17 @@
 from itertools import *
 
+def make_iterables_to_chain():
+    yield [1,2,3]
+    yield ['a', 'b', 'c']
+
 def test_chain():
     tmp=[]
     for i in chain([1, 2, 3],['a','b','c']):
         tmp.append(  i )
     assert tmp == [ 1,2,3,'a','b','c']
+
+def test_chain_from_iterable():
+    tmp = []
+    for i in chain.from_iterable(make_iterables_to_chain()):
+        print (i, end=' ')
+    assert tmp == True
