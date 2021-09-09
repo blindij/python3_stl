0e556186c9aa7f9148f55ad5014e2e85403ad345
Bjorn Lindi <blindij@users.noreply.github.com>
2021-09-09 15:21:18 +0200

    Print October with weeks starting Monday

--- date_and_time/src/calendar_textcalendar.py
+++ date_and_time/src/calendar_textcalendar.py
@@ -4,3 +4,5 @@ import calendar
 # a week on Monday. The example show weeks starting on Sunday
 c = calendar.TextCalendar(calendar.SUNDAY)
 c.prmonth(2021,9)
+d = calendar.TextCalendar()
+d.prmonth(2021,10)
